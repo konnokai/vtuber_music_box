8b0eea9b838cb8d29993bf6a0a7dfaf45d37ac0d
孤之界 <f20001012@gmail.com>
2023-11-21 18:26:45 +0800

    feature: 新增 vue-youtube 套件

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,30 @@
-<script setup lang="ts">
-</script>
-
 <template>
+  <div ref="youtube" />
 </template>
 
-<style scoped>
-</style>
+<script setup lang="ts">
+import { usePlayer } from '@vue-youtube/core';
+import { ref } from 'vue';
+
+const videoId = ref('bmSkGGTcIx8');
+const youtube = ref();
+
+const { onReady } = usePlayer(videoId, youtube, {
+  cookie: true,
+  playerVars: {
+    mute: 0,
+    enablejsapi: 1,
+    listType: 'user_uploads',
+  },
+});
+
+onReady(event => {
+  setTimeout(() => {
+    console.log('1. ' + event.target.getVideoUrl());
+    event.target.cueVideoById('ytJ_yfyELm4', 5, 'default');
+    setTimeout(() => {
+      console.log('2. ' + event.target.getVideoUrl());
+    }, 3 * 100);
+  }, 5 * 1000);
+});
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { createManager } from '@vue-youtube/core';
 import './style.css';
 import App from './App.vue';
 
 const app = createApp(App);
 
 app.use(createPinia());
+app.use(createManager());
 app.mount('#app');
