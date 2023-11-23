16fdcba386ee0188ba122e7111575494cdaa32b4
Hank Yang <86253725+SheepNDW@users.noreply.github.com>
2023-11-23 13:56:13 +0800

    feat: add unplugin-icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'url';
+import Icons from 'unplugin-icons/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +12,10 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Icons({
+      autoInstall: true,
+    }),
+  ],
 });
