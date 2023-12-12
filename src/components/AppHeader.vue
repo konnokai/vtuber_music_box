fa7b8736dedd6408d928dd22023ffbf76c86a353
Hank Yang <86253725+SheepNDW@users.noreply.github.com>
2023-12-12 09:02:00 +0800

    feat: basic layout

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header
+    class="static w-full border-b-2 border-gray-400/50 bg-[#2e2d35] px-2.5 py-4 text-white md:px-8 lg:px-14"
+  >
+    <div class="flex justify-between">
+      <h1 class="text-3xl">VTuber Music Box</h1>
+      <div class="flex gap-4 text-3xl">
+        <i-mdi-youtube class="cursor-pointer" />
+        <i-mdi-github class="cursor-pointer" />
+      </div>
+    </div>
+  </header>
+</template>
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'url';
 import Icons from 'unplugin-icons/vite';
+import IconsResolver from 'unplugin-icons/resolver';
+import Components from 'unplugin-vue-components/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,6 +16,9 @@ export default defineConfig({
   },
   plugins: [
     vue(),
+    Components({
+      resolvers: [IconsResolver()],
+    }),
     Icons({
       autoInstall: true,
     }),
