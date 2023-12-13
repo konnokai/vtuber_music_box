e540340c0237f1b7cd7e36ef1a11a48998a107df
Hank Yang <86253725+SheepNDW@users.noreply.github.com>
2023-12-13 16:09:20 +0800

    feat: add environment variables for dev and production

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_DISCORD_CLIENT_ID: string;
+  readonly VITE_API_PATH: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
